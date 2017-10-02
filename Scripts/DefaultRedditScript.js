a8712a755e269e31e47bf39c373f682c487bb464
orian7 <32451686+orian7@users.noreply.github.com>
2017-10-02 15:43:20 +0300

    Add files via upload

--- Scripts/DefaultRedditScript.js
+++ Scripts/DefaultRedditScript.js
@@ -2,6 +2,7 @@
 //function for triggering search on a particular subreddit that the user has entered as input
 function enterKeyPress(event) {
   event = event || window.event;
+
   //in case that the user pressed the enter key
     if (event.keyCode == 13)
     {
@@ -15,11 +16,13 @@ function redditDisplay() {
   //extricate the subreddit for the search of its pictures
   var subReddit = getSubreddit();
     var wall = document.querySelector(".wall");
+
     //extricate 20 pictures for the gallery from the reddit site
     reddit.top(subReddit).limit(20).fetch(function (res) {
       //loop over the data that the reddit returned
       for (var i = 0; i < res.data.children.length; i++) {
         var data = res.data.children[i].data;
+
         //creating a lovely gallery item
         var galleryItem = document.createElement("div");
         var link = document.createElement("a");
@@ -28,6 +31,7 @@ function redditDisplay() {
         var description = document.createElement("div");
         //creating the image element
         var thumbnail = document.createElement("img");
+
         //set the link for the reddit's appropriate post
         link.setAttribute("href", "https://www.reddit.com" + data.permalink);
         thumbnail.setAttribute("src", data.thumbnail);
@@ -35,9 +39,11 @@ function redditDisplay() {
         //set the post's title
         description.innerText = data.title;
         middle.className = "middle";
+
         //trigger openning the post in a new tab
         link.target = "_blank";
         galleryItem.className = "gallery";
+
         //appending elements to our wonderful gallery
         galleryItem.appendChild(link);
         link.appendChild(thumbnail);
@@ -45,24 +51,29 @@ function redditDisplay() {
         link.appendChild(middle);
         wall.appendChild(galleryItem);
       }
+
   }, function(err) {
     //informing the user in case that the reddit returned an error
     alert("Reddit returned an error :(");
   });
 }
+
 //a function for define the required subreddit for the gallery
 function getSubreddit() {
   var subReddit = document.getElementById("search").value;
   //in case that there is no input from the user
+
   if (subReddit == undefined || subReddit == "") {
     //define a default subreddit
     subReddit = 'funny';
   }
   return subReddit;
 }
+
 //a function for clearing the page from pictures
 function clearDisplay() {
   var myNode = document.querySelector(".wall");
+  
   //remove all the gallery wall's children (i.e. all the pictures)
   while (myNode.firstChild) {
     myNode.removeChild(myNode.firstChild);
