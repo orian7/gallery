f7f2e1d6b66c1dd1b98e4f847e1cbd3f67413fb8
orian7 <32451686+orian7@users.noreply.github.com>
2017-10-02 15:56:40 +0300

    Add files via upload

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,5 +1,5 @@
 import "!style-loader!css-loader!../Styles/Style.css"
-//require('./DefaultRedditScript.js');
-//require('./BarScript.js');
+require('./DefaultRedditScript.js');
+require('./BarScript.js');
 require('../Styles/Style.css');
 require('../webpack.config.js');
